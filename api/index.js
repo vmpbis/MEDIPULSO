7f6c5da83bbd04a85b2f58f400c3feeaba9f0c6d
vmpbis <remotasko31@gmail.com>
2024-04-05 16:48:15 +0200

    mongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+// Load environment variables
+dotenv.config()
+
+// Connect to MongoDB
+mongoose
+    .connect( process.env.MONGODB_CONNECTING_STRING)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    }).catch((err) => {
+    console.log('Failed to connect to MongoDB', err)
+})
+
+// Create an Express app
 const app = express()
 
+//listen to port 6000
 app.listen(6000, () => {
   console.log('Server is running on port 6000')
 })
