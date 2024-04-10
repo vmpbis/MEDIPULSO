922f2d436ea8f4efe75bdb16aefd563e9741d4df
vmpbis <remotasko31@gmail.com>
2024-04-10 18:15:53 +0200

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
 // Load environment variables
 dotenv.config()
@@ -18,6 +19,9 @@ mongoose
 const app = express()
 
 //listen to port 6000
-app.listen(6000, () => {
-  console.log('Server is running on port 6000')
+app.listen(7500, '0.0.0.0', () => {
+  console.log('Server is running on port 7500')
 })
+
+app.use('/api/user', userRoutes)
+
