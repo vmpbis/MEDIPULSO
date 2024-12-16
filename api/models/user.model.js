9e2e762e1a21c23de8367ff59f4bba339c23b446
vmpbis <remotasko31@gmail.com>
2024-12-16 13:29:45 +0100

    user schema update, roles implemented

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -40,6 +40,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
+    role: {
+        type: String,
+        enum: ['user', 'doctor', 'clinic'],
+        default: 'user',
+    },
+    isLoggedIn: {
+        type: Boolean,
+        default: false,
+    },
     
 }, {timestamps: true}, // this will add createdAt and updatedAt fields) in the database
 
