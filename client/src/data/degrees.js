ace538e4b9376ba63550d6389c6861218800bdf0
vmpbis <remotasko31@gmail.com>
2024-12-18 22:35:40 +0100

    doctor diseases data

---
+++ client/src/data/degrees.js
@@ -0,0 +1,19 @@
+export const degrees = [
+    "bow. lek. med.",
+    "Dr. of Medicine",
+    "Dr. of Dentistry",
+    "Dr. hab. n. med.",
+    "Prof. dr hab. n. med.",
+    "Dentist",
+    "Veterinarian",
+    "Dr.",
+    "M.Sc.",
+    "Dr.n o zdr. publ.",
+    "Msc Eng.",
+    "Dr. Eng.",
+    "Prof. dr hab. inż.",
+    "Full Professor",
+    "PhD in Social Sciences",
+    "PhD in Humanities",
+    "University Professor"
+  ]
---
+++ client/src/data/diseases.js
@@ -0,0 +1,14 @@
+ export  const diseases = [
+    "Arterial hypertension",
+    "Diabetes",
+    "Coronary heart diseases",
+    "Heart attack",
+    "Cardiac arrhythmias",
+    "Heart failure",
+    "Coronary artery disease",
+    "Hypertension",
+    "Heart diseases",
+    "Heart defects"
+]
+
+//export default diseases
