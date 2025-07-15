773a494df97d5b34ccf77945814a3517f76175e3
vmpbis <remotasko31@gmail.com>
2025-07-15 10:59:42 +0200

    clinic route functionality

--- api/routes/clinic.route.js
+++ api/routes/clinic.route.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { test } from '../controllers/clinic.controller.js'
+import { logout } from '../controllers/auth.controller.js'
+
 
 const router = express.Router()
 
 router.use('/test', test)
+router.post('/logout', logout)
 
 export default router
