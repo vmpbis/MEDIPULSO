8e8bfec3f62670455e49715ec809491a7f6a211f
vmpbis <remotasko31@gmail.com>
2025-07-15 09:53:23 +0200

    patient appointment model functionality

---
+++ api/models/appointment.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose"
+
+const appointmentSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", 
+        required: true,
+    
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "DoctorForm", 
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: false,
+    },
+    lastName: {
+        type: String,
+        required: false,
+    },
+    email: {
+        type: String,
+        required: false,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    consent: {
+        type: Boolean,
+        required: true,
+    },
+    reason: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["pending", "confirmed", "canceled", "completed"],
+        default: "pending",
+    },
+    specialNotes: {
+        type: String,
+        required: false,
+        default: "",
+    }, 
+},{timestamp: true})
+
+const Appointment = mongoose.model("Appointment", appointmentSchema)
+
+export default Appointment
