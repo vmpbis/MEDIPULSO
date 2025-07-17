523feda06a15fc103e6a1396a07ddd737f9b503c
vmpbis <remotasko31@gmail.com>
2025-07-17 16:43:06 +0200

    feat(form): add availability step to doctor profile completion

---
+++ client/src/components/doctor/DoctorAvailability.jsx
@@ -0,0 +1,23 @@
+
+import DoctorCalendar from './DoctorCalendar'
+
+const DoctorAvailability = ({isSubmitting, handleSubmit, errorMessage, successMessage, handleBack, setAvailability})=>  {
+  return (
+    <div>
+        <DoctorCalendar />
+        {errorMessage && <p className='text-red-500 text-center'>{errorMessage}</p>}
+        {successMessage && <p className='text-green-500 text-center'>{successMessage}</p>}
+        <div className='flex justify-between'>
+                    <button onClick={handleBack} className='bg-[#00c3a5] text-white py-2 px-4 rounded mt-4 ml-4'>
+                    Back
+                  </button>
+                  <button onClick={handleSubmit} className='bg-[#00c3a5] text-white py-2 px-4 rounded mt-4 mr-4' disabled={isSubmitting}>
+                    {isSubmitting ? 'Submitting...' : 'Submit'}
+                  </button>
+                 
+                </div>
+    </div>
+  )
+}
+
+export default DoctorAvailability
