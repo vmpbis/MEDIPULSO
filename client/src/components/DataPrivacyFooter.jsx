069c65801db8985089641fb07d680978aaf9de1e
vmpbis <remotasko31@gmail.com>
2025-07-18 06:38:09 +0200

    data privacy footer

---
+++ client/src/components/DataPrivacyFooter.jsx
@@ -0,0 +1,17 @@
+function DataPrivacyFooter() {
+  return (
+    <div className='bg-[#00b39be6] '>
+        <div className='lg:w-[70%] p-8  m-auto lg:text-center text-white py-24'>
+            <h2 className='text-2xl mb-6'>
+                We will be happy to clarify all issues.
+            </h2>
+           
+            <p>If you have any questions or concerns regarding the methods of securing or processing data on our website,<br /> please contact us by e-mail at info@medipulse.pl.</p>
+          
+            <button className='mt-6 bg-blue-500 px-4 py-3'>info@medipulse.pl</button>
+        </div>
+    </div>
+  )
+}
+
+export default DataPrivacyFooter
