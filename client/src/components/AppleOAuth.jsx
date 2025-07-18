85c5c96e0d77cf6ae125b4ef04bb0764012db562
vmpbis <remotasko31@gmail.com>
2025-07-18 06:29:16 +0200

    Apple OAuth functionality

--- client/src/components/AppleOAuth.jsx
+++ client/src/components/AppleOAuth.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+
 import { AiFillApple } from "react-icons/ai"
-import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { appleSignIn } from '../redux/user/userSlice'
 
@@ -19,8 +18,6 @@ const AppleOAuth = () => {
                 <button
                     className='w-full flex justify-center items-center py-2 px-4 rounded-sm bg-[#000] text-white text-sm font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-[#000] focus:ring-[#000] hover:bg-[#000] hover:text-white hover:shadow-lg transition duration-300 ease-in-out'
                     type='button'
-                    // gradientDuoTone='pinkToOrange'
-                    // outline
                 >
                     <AiFillApple className='w- h-6 mr-2'/>
                     <span className="text-center">{loading ? 'Continue...' : 'Continue with Apple'}</span>
